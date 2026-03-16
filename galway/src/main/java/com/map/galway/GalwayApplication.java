612001b56c32cda193366afc9030451abf584691
Rostyslav Kholod <kholod.rostik@gmail.com>
2026-03-17 01:48:49 +0200

    Push start of the project

---
+++ galway/src/main/java/com/map/galway/GalwayApplication.java
@@ -0,0 +1,13 @@
+package com.map.galway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalwayApplication.class, args);
+	}
+
+}
---
+++ galway/src/test/java/com/map/galway/GalwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.galway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
